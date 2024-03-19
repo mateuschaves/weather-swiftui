42b006e5c44536caaeca362988049a900d84533e
Mateus Henrique <mateushenriquechaves1@gmail.com>
2024-03-18 23:37:21 -0300

    Initial Commit

---
+++ goodweather/goodweatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  goodweatherApp.swift
+//  goodweather
+//
+//  Created by Mateus Henrique on 18/03/24.
+//
+
+import SwiftUI
+
+@main
+struct goodweatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
